1127bc7c3dceafd6231c5be0fe2f2a6b77f362e0
Juanfle <juanfleming2006@gmail.com>
2025-10-15 13:38:59 -0300

    hola como estan

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("hola")
+alert("mensaje de alerta")
+prompt("ingrese algun prompt")
+let variable
+variable = "hola"
+alert (variable)
+variable = "chau"
+alert (variable)
+
+variable = prompt("ingrese su nombre")
+alert ("su nombre es " + variable)
---
+++ app2.js
@@ -0,0 +1,14 @@
+
+let numeroSecreto = 3;
+let intento
+
+while (intento != numeroSecreto) {
+
+    intento = prompt("Adivina el numero")
+
+    if (intento == numeroSecreto) {
+        alert("Felicidades, adivinaste")
+    } else {
+        alert("Intenta nuevamente")
+    }
+}
